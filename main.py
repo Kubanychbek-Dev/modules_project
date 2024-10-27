f1d39e91ab1562d262b6a77221207f28c4aa14a2
Kubanychbek Tursunbekov <kubanychbek.tursunbekov.alt@gmail.com>
2024-10-27 21:56:07 +0300

    add main.py and make import from modules to main

---
+++ main.py
@@ -0,0 +1,6 @@
+from my_folder import module_1, module_2
+from my_package.package_folder_1 import module_3, module_4
+from my_package.package_folder_1.package_folder_2 import module_5, module_6
+from my_package.package_folder_1.package_folder_2.package_folder_3 import module_7, module_8
+
+
