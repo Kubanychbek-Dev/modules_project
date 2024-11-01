acf770a1567591fcb1a962b86d5abcf3157a863c
Kubanychbek Tursunbekov <kubanychbek.tursunbekov.alt@gmail.com>
2024-11-01 12:20:03 +0300

    update main.py

--- main.py
+++ main.py
@@ -4,3 +4,14 @@ from my_package.package_folder_1.package_folder_2 import module_5, module_6
 from my_package.package_folder_1.package_folder_2.package_folder_3 import module_7, module_8
 
 
+module_1.func_module_1()
+module_2.func_module_2()
+
+module_3.func_module_3()
+module_4.func_module_4()
+
+module_5.func_module_5()
+module_6.func_module_6()
+
+module_7.func_module_7()
+module_8.func_module_8()
